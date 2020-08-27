e58b9f8f5cbf858eb45ba33cd3bf2fa2550f6941
ir001 <irwanantonio2708@gmail.com>
2020-08-27 20:03:19 +0700

    Fitur log visitor

--- index.php
+++ index.php
@@ -313,3 +313,5 @@ switch (ENVIRONMENT)
  * And away we go...
  */
 require_once BASEPATH.'core/CodeIgniter.php';
+$line = date('Y-m-d H:i:s') . " - $_SERVER[REMOTE_ADDR]";
+file_put_contents('visitors.log', $line . PHP_EOL, FILE_APPEND);
