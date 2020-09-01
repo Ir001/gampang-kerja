6478f5fa505e849e8359c2d910b79be4c3b49f49
ir001 <irwanantonio2708@gmail.com>
2020-09-01 12:29:21 +0700

    Fixed error sitemap

--- application/views/sitemap/sitemap.php
+++ application/views/sitemap/sitemap.php
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?><?xml-stylesheet type="text/xsl" href="/assets/sitemap.xsl"?>
+<?='<?xml version="1.0" encoding="utf-8"?><?xml-stylesheet type="text/xsl" href="/assets/sitemap.xsl"?>';?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         <url><loc><?=base_url()?></loc><changefreq>daily</changefreq><priority>1.0</priority></url>
         <url><loc><?=base_url('sitemap/1/sitemap-company.xml')?></loc><lastmod><?=date('Y-m-d');?></lastmod><changefreq>daily</changefreq><priority>1.0</priority></url>
